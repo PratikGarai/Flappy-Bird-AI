becfc0318734ab5799a2ef1a657cf774aece3241
PratikGarai <pratikgarai0208@gmail.com>
2020-08-31 12:51:54 +0530

    Added the basic implementation

--- Main.py
+++ Main.py
@@ -4,5 +4,34 @@ import time
 import os
 import random
 
-WINDOW_WIDTH = 600
+import Bird 
+import Pipe
+import Terrain
+
+WINDOW_WIDTH = 500
 WINDOW_HEIGHT = 800
+
+def draw_window(win, bird):
+    win.blit(Terrain.BG_IMG, (0,0))
+    bird.draw(win)
+    pygame.display.update()
+
+def main():
+    bird = Bird.Bird(200,200)
+    win = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    clock = pygame.time.Clock()
+
+    run = True
+    while run :
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                run = False
+        bird.move()
+        draw_window(win, bird)
+
+    pygame.quit()
+    quit()
+
+if __name__=='__main__':
+    main()
